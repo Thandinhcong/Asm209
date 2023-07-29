068ed4d7c83e5b846e2c766476dd00aea50e6556
thandinhjs <dinhcongthan03@gmail.com>
2023-07-29 10:14:27 +0700

    create footerpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css";
+import NotFoundPage from './pages/notFound';
 
 function App() {
 
-  return (
-    <div>helllo</div>
-  )
+  <BrowserRouter>
+    <Routes>
+      <Route path='home' element />
+    </Routes>
+    <Route path='*' element={<NotFoundPage />} />
+  </BrowserRouter>
 }
 
 export default App
