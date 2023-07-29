cc48e292464b49e6b540baa8f3eb517a4dedb6c6
thandinhjs <dinhcongthan03@gmail.com>
2023-07-29 10:29:15 +0700

    header

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css";
 import NotFoundPage from './pages/notFound';
+import Layout from './pages/clients/HomePage';
 
 function App() {
-
-  <BrowserRouter>
-    <Routes>
-      <Route path='home' element />
-    </Routes>
-    <Route path='*' element={<NotFoundPage />} />
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
--- src/pages/clients/HomePage/index.tsx
+++ src/pages/clients/HomePage/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Footer from '../Footer'
-
-
+import "../../../css/bootstrap-icons.css"
+import "../../../css/bootstrap.min.css"
+import "../../../css/teamplatemo-topic-listing.css"
+import Header from '../Header'
 const Layout = () => {
     return (
         <div>
+            <Header />
             <Footer />
         </div>
     )
